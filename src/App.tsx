08d82b7a3989d8fa93de976f934a851e5260c10e
keshricoder <keshailesh98@gmail.com>
2024-01-13 22:16:45 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+import './styles/tailwind.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      {/* Your portfolio components go here */}
+    </div>
+  );
+};
+
+export default App;
